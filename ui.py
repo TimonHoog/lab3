9fe01ce5a9c798727ca7f487a30b0b81a2cfd914
oli4-004 <oliviervanmierlo2004@gmail.com>
2024-01-22 11:57:30 +0100

    ultimate commitment

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ from debitcard import DebitCard
 from coin_machine import IKEAMyntAtare2000
 from ui_info import UIPayment, UIClass, UIWay, UIDiscount, UIPayment, UIInfo
 
+import abc as ABC
 
 class UI(tk.Frame):
 
@@ -14,52 +15,66 @@ class UI(tk.Frame):
 		self.widgets()
 
 	def handle_payment(self, info: UIInfo):
-
 		# **************************************
 		# Below is the code you need to refactor test
 		# **************************************
+		class Oracle_Database():
+			global price
+			
+			# get number of tariefeenheden
+			tariefeenheden: int = Tariefeenheden.get_tariefeenheden(info.from_station, info.to_station)
 
-		# get number of tariefeenheden
-		tariefeenheden: int = Tariefeenheden.get_tariefeenheden(info.from_station, info.to_station)
+			# compute the column in the table based on choices
+			table_column = 0
+			if info.travel_class == UIClass.FirstClass:
+				table_column = 3
 
-		# compute the column in the table based on choices
-		table_column = 0
-		if info.travel_class == UIClass.FirstClass:
-			table_column = 3
+			# then, on the discount
+			if info.discount == UIDiscount.TwentyDiscount:
+				table_column += 1
+			elif info.discount == UIDiscount.FortyDiscount:
+				table_column += 2
 
-		# then, on the discount
-		if info.discount == UIDiscount.TwentyDiscount:
-			table_column += 1
-		elif info.discount == UIDiscount.FortyDiscount:
-			table_column += 2
+			# compute price
+			price: float = PricingTable.get_price (tariefeenheden, table_column)
+			if info.way == UIWay.Return:
+				price *= 2
 
-		# compute price
-		price: float = PricingTable.get_price (tariefeenheden, table_column)
-		if info.way == UIWay.Return:
-			price *= 2
+		class Payment_System(ABC):
+			# pay
+			def __init__(self):
+				pass
+			def choose(self, info.payment):
+				if info.payment == UIPayment.CreditCard:
+					Credit_Card()
+				elif info.payment == UIPayment.DebitCard:
+					Debit_Card()
+				elif info.payment == UIPayment.Cash:
+					Cash_Payment()
 
-		# add 50 cents if paying with credit card
-		if info.payment == UIPayment.CreditCard:
-			price += 0.50
-		
-		# pay
-		if info.payment == UIPayment.CreditCard:
-			c = CreditCard()
-			c.connect()
-			ccid: int = c.begin_transaction(round(price, 2))
-			c.end_transaction(ccid)
-			c.disconnect()
-		elif info.payment == UIPayment.DebitCard:
+		class Cash_Payment(Payment_System):
+			coin = IKEAMyntAtare2000()
+			coin.starta()
+			coin.betala(int(round(price * 100)))
+			coin.stoppa()
+
+		class Debit_Card(Payment_System):
 			d = DebitCard()
 			d.connect()
 			dcid: int = d.begin_transaction(round(price, 2))
 			d.end_transaction(dcid)
 			d.disconnect()
-		elif info.payment == UIPayment.Cash:
-			coin = IKEAMyntAtare2000()
-			coin.starta()
-			coin.betala(int(round(price * 100)))
-			coin.stoppa()
+
+		class Credit_Card(Payment_System):
+			c = CreditCard()
+			c.connect()
+			ccid: int = c.begin_transaction(round(price, 2))
+			c.end_transaction(ccid)
+			c.disconnect()
+			
+			# add 50 cents if paying with credit card
+			if info.payment == UIPayment.CreditCard:
+				price += 0.50
 
 #region UI Set-up below -- you don't need to change anything
 

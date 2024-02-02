0dea0e0b92b4bc6cf88c628d168d90f07f494c77
oli4-004 <oliviervanmierlo2004@gmail.com>
2024-02-02 14:31:45 +0100

    quick clean

--- ticket_printer.py
+++ ticket_printer.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from ui_info import UIInfo
 from tkinter import messagebox
 
-#inheritance with vending machine logic
+# inheritance with vending machine logic
 class Ticket_Printer:
     def __init__(self, info: UIInfo, prijs):
         self.info = info
@@ -10,29 +10,38 @@ class Ticket_Printer:
         pass
     
     def print_ticket(self, info, prijs):
+        # for now it will print the day the ticket is printed
+        # https://stackoverflow.com/q/32490629
+        ticket_date = datetime.today().strftime('%d-%m-%Y')
+        
         # the GUI only allows for one ticket to be printed, this code allows the 1 to be changed to a variable
         to_print = 1
         price = prijs
         carriage_class = info.travel_class
-        # for now it will print the day the ticket is printed
-        # https://stackoverflow.com/q/32490629
-        ticket_date = datetime.today().strftime('%d-%m-%Y')
         return_ticket = info.way
         starting_location = info.from_station
         ending_location =  info.to_station
         discount = info.discount
-        paymentsystem = info.payment
+
         # rename the boolean value to an understandable, printable one
         if return_ticket == 2:
             return_ticket = 'Return trip'
         elif return_ticket == 1:
             return_ticket = 'Single trip'
+
+        # same for the discounts        
         if discount == 1:
             discount = 'No discount applied'
         elif discount == 2:
             discount = '20% discount applied'
         elif discount == 3: 
             discount = '40% discount applied'
+        
+        # This is to display the correct price including the supplement on the ticket
         if info.payment == 2:
             price = float(price) + 0.5
-        messagebox.showinfo(title="Ticket", message = f"Print date: {ticket_date}\nTicket amount: {to_print}\nTicket cost:{price} euro\nApplied discount: {discount}\nYou are travelling {carriage_class} class\nThe ticket is valid on: {ticket_date}\n Ticket type: {return_ticket}\nFrom: {starting_location}\nTo: {ending_location}")
+
+        messagebox.showinfo(
+            title="Ticket", 
+            message = f"Print date: {ticket_date}\nTicket amount: {to_print}\nTicket cost:{price} euro\nApplied discount: {discount}\nYou are travelling {carriage_class} class\nThe ticket is valid on: {ticket_date}\n Ticket type: {return_ticket}\nFrom: {starting_location}\nTo: {ending_location}"
+            )
